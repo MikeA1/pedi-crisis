8c7cb8de4e1af6fc38b6d8091ce90690c1f9ba1e
Mike Anderson <MikeA1@users.noreply.github.com>
2018-04-19 16:28:48 -0600

    Phone numbers now appear in events

--- www/js/index.js
+++ www/js/index.js
@@ -215,6 +215,9 @@
                                 style.top = page.offsetTop + "px";
                                 style.position = "absolute";
                                 // Note: This `.page` already has values for `bottom` and `overflow-y` via CSS rules.
+
+                                // 
+                                app.phoneNumbers.updateElements(page);
                             }
                         }
                     };
@@ -437,5 +440,67 @@
             item.content.style.display = "none";
         });
     };
+
+    // Purpose: store phone numbers across app sessions via local storage. These phone numbers are used throughout the app.
+    // The "phoneNumbers" data structure  is persisted in local storage as an associative array (i.e., "dictionary")
+    // keyed on `description`:
+    // {
+    //   "Fire": {
+    //     "description": "Fire",
+    //     "number": "Yell Loudly",
+    //     "isHyperlink": false
+    //   },
+    //   "My Custom Number": {
+    //     "description": "My Custom Number",
+    //     "number": "555-555-5555",
+    //     "isHyperlink": true
+    //   }
+    // }
+    app.phoneNumbers = {
+        /***
+         * Simple helper for retrieving phone numbers.
+         */
+        get: () => {
+            const stringData = localStorage.getItem("phone-numbers");
+            return stringData ? JSON.parse(stringData) : {};
+        },
+        /***
+         * Simple helper for saving phone numbers. Note that the value must already be in the correct format!
+         */
+        set: (phoneNumbers) => {
+            if (phoneNumbers.length) {
+                console.warn("Oops - phoneNumbers should be an associative array.");
+            }
+            localStorage.setItem("phone-numbers", JSON.stringify(phoneNumbers));
+        },
+        /**
+         * This updates all DOM elements that have the attribute "data-phone-number" defined.
+         * @argument parent HTMLElement is an optional parameter that restricts the DOM search for 
+         * elements with the attribute "data-phone-number";
+         */
+        updateElements: (parent) => {
+            Array.from((parent ||document).querySelectorAll("[data-phone-number]")).forEach(element => {
+                const storedPhoneNumbers = localStorage.getItem("phone-numbers");
+                const phoneNumbers = storedPhoneNumbers !== null ? JSON.parse(storedPhoneNumbers) : {};
+                const description = element.dataset.phoneNumber; // Note the automatic conversion from "phone-number" to "phoneNumber"!
+                const phoneNumber = phoneNumbers[description];
+                if (phoneNumber !== undefined && phoneNumber.number !== undefined) {
+                    const number = phoneNumber.number.trim();
+                    if (phoneNumber.isHyperlink) {
+                        const link = document.createElement("a");
+                        link.href="tel:" + number;
+                        link.textContent = " #" + number;
+                        element.appendChild(link);
+                    } else {
+                        const span = document.createElement("span");
+                        span.textContent = " #" + number;
+                        element.appendChild(span);
+                    }
+                }
+            });
+        },
+        systemDescriptions: {"Code Team": "Code Team", "PICU": "PICU", "Fire": "Fire", "Overhead STAT": "Overhead STAT", "ECMO": "ECMO"}
+    };
+
 
 })();
