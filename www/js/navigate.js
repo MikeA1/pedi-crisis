c68223137f64fd40298498d1ccd1684d3ebd6d4e
Mike Anderson <MikeA1@users.noreply.github.com>
2018-08-21 11:18:31 -0600

    Changed title from "Pedi Crisis" to "Pedi Crisis 2.0"

--- www/js/navigate.js
+++ www/js/navigate.js
@@ -291,10 +291,10 @@
                 const nextHeader = header || defaultHeader;
                 if (nextHeader === "Pedi Crisis") {
                     headerElement.textContent = "Pedi Crisis ";
-                    // const sup = document.createElement("sup");
-                    // sup.textContent = "beta";
-                    // sup.style.color = "#888";
-                    // headerElement.appendChild(sup);
+                    const sup = document.createElement("sup");
+                    sup.textContent = "2.0";
+                    sup.style.color = "#888";
+                    headerElement.appendChild(sup);
                 } else {
                     headerElement.textContent = nextHeader;
                 }
